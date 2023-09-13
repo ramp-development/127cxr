4f008e0bf62a7d86d6245bd0f349251ab627fbb5
g4knr <85592362+g4knr@users.noreply.github.com>
2023-09-13 13:29:51 +0100

    Updated the stupid splitLines

--- src/animations/utils/splitLines.ts
+++ src/animations/utils/splitLines.ts
@@ -2,18 +2,28 @@ import { SplitText } from 'gsap/SplitText';
 
 export const splitLines = (element: Element | Element[], timeline: GSAPTimeline, delay: string) => {
   const formatted = new SplitText(element, {
-    type: 'lines, words',
+    type: 'lines',
     linesClass: 'overflow-hidden',
   });
 
-  formatted.lines.forEach((line, index) => {
-    timeline.from(
-      line.children,
-      {
-        translateY: '100%',
-        // stagger: 0.01,
-      },
-      index === 0 ? delay : '<0.05'
-    );
-  });
+  timeline.from(
+    formatted.lines,
+    {
+      translateY: '1rem',
+      opacity: 0,
+      stagger: 0.01,
+    },
+    delay
+  );
+
+  // formatted.lines.forEach((line, index) => {
+  //   timeline.from(
+  //     line.children,
+  //     {
+  //       translateY: '100%',
+  //       // stagger: 0.01,
+  //     },
+  //     index === 0 ? delay : '<0.05'
+  //   );
+  // });
 };
